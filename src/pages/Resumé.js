f61db60bbdc2155ac4dad071a5b474bb4eda0eff
Sam-Cordry <samcordry@gmail.com>
2022-12-16 17:32:53 -0500

    Added resumé page

--- src/pages/Resumé.js
+++ src/pages/Resumé.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Cordry_Resume from "../Cordry_Resume.pdf"
 
 export default function Resumé() {
     return (
         <div className="resumé">
             <h1>Resumé</h1>
+            <iframe src={Cordry_Resume} title="Cordry_Resumé" width="75%" height="800px"></iframe>
         </div>
     )
 }

551fb609938d3ee4f12423cee3cc8e9c5bfcb6ea
Sam-Cordry <samcordry@gmail.com>
2022-12-16 08:39:41 -0500

    Basic navbar created

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h2>Sam Cordry</h2>
+            <h3 className="navbar-about">About Me</h3>
+            <h3 className="navbar-projects">Projects</h3>
+            <h3 className="navbar-resumé">Resumé</h3>
+            <h3 className="navbar-contact">Contact Me</h3>
+        </div>
+    )
+}
